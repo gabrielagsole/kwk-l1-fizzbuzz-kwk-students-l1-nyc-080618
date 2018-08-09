c3c41812f182f2fd1f0de18dedabe6438098bce8
gabrielagsole <ggarciasole@gmail.com>
2018-08-09 17:24:14 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,8 @@
+sports = ["basketball", "baseball", "football"]
+
+  num = 0 
+  
+sports.each do |sporty|
+  num += 1 
+  puts "Sport #{num}: #{sporty}"
+end
---
+++ dance.rb
@@ -0,0 +1,46 @@
+def shake_it_off 
+  puts "cause the player's gonna"
+  play_count = 0 
+  while play_count < 5
+  puts "play"
+  play_count += 1
+end 
+end 
+def shake_it_off_2 
+  puts "and the hater's gonna"
+  hate_count = 0 
+  while hate_count < 5
+  puts "hate"
+  hate_count += 1 
+end
+end 
+def shake_it_off_3
+  puts "but i'm just gonna"
+  shake_count = 0 
+  while shake_count < 5
+  puts "shake"
+  shake_count += 1
+end
+end 
+def shake_it_off_4
+  shakeitoff_count = 0 
+  while shakeitoff_count < 2
+  puts "shake it off!"
+  shakeitoff_count += 1
+end
+end
+
+def shakeitoff_chorus
+  shakeitoff_chorus_count = 0 
+  while shakeitoff_chorus_count < 5
+  shake_it_off
+  shake_it_off_2
+  shake_it_off_3
+  shake_it_off_4
+  shakeitoff_chorus_count += 1
+ end  
+end
+
+shakeitoff_chorus
+
+
